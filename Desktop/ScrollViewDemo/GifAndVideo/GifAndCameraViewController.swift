c60695dea197fdace488287b90e015db84959f3d
Nguyễn Thành Đạt <dat.nguyen.thanh@ntq-solution.com.vn>
2018-10-16 19:21:51 +0700

    Them chuc nang moi phan video

--- Desktop/ScrollViewDemo/GifAndVideo/GifAndCameraViewController.swift
+++ Desktop/ScrollViewDemo/GifAndVideo/GifAndCameraViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 import AVFoundation
 import AVKit
 import Photos
+import SwiftMessages
+import NVActivityIndicatorView
+import MobileCoreServices
+
+var urlGifString = ""
 
 class GifAndCameraViewController: UIViewController {
      var urlString = ""
+    var checkCreateVideoAndGit = 0
+    @IBOutlet weak var btnReset: UIButton!
+    @IBOutlet weak var btnShareTo: UIButton!
+    @IBOutlet weak var btnSaveCameraRoll: UIButton!
+    @IBOutlet weak var createGif: UIButton!
+    @IBOutlet weak var createVideo: UIButton!
+    @IBOutlet weak var videogif: UIImageView!
+    @IBOutlet weak var viewShowVideoAndGif: UIView!
 
+    @IBOutlet weak var loadingView: NVActivityIndicatorView!
     @IBOutlet weak var sliderSetValue: UISlider!
     @IBOutlet weak var lblShowValueSlider: UILabel!
     @IBOutlet weak var viewPresent: UIView!
@@ -22,7 +36,7 @@ class GifAndCameraViewController: UIViewController {
     @IBOutlet weak var constrainViewGifandVideo: NSLayoutConstraint!
     @IBOutlet weak var stackTimeSpeedHidden: UIStackView!
     @IBOutlet weak var constrainSpeed: NSLayoutConstraint!
-    @IBOutlet weak var btnReset: UIButton!
+
     @IBOutlet weak var constrainReset: NSLayoutConstraint!
     @IBOutlet weak var constrainTopSpeed: NSLayoutConstraint!
     @IBOutlet weak var constrainBottomSpeed: NSLayoutConstraint!
@@ -35,18 +49,38 @@ class GifAndCameraViewController: UIViewController {
     var imageArrayToVideoURL = NSURL()
     let audioIsEnabled: Bool = false //if your video has no sound
     var asset: AVAsset!
+    var numberValueSlider = 0
 
     var videoPlayerVc : AVPlayerViewController!
-    @IBOutlet weak var videogif: UIImageView!
+
 
     var arrimageVideo:[UIImage] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Gif & Video"
-        lblShowValueSlider.text = "3"
         btnReset.isHidden = true
         constrainReset.constant = 0
+        UserDefaults.standard.removeObject(forKey: "sliderValue")
+        lblShowValueSlider.text = "3"
+        let imag = UIImage()
+        imag.animatedGif(from: arrimageVideo)
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+                self.title = "Gif & Video"
+        btnReset.layer.cornerRadius = btnReset.frame.height / 2
+        btnShareTo.layer.cornerRadius = btnShareTo.frame.height / 2
+        btnSaveCameraRoll.layer.cornerRadius = btnSaveCameraRoll.frame.height / 2
+        createGif.layer.cornerRadius = 7
+        createVideo.layer.cornerRadius = 7
+        viewShowVideoAndGif.layer.cornerRadius = 25
+        viewShowVideoAndGif.clipsToBounds = true
+        if let setValueSlider = UserDefaults.standard.object(forKey: "sliderValue") as? Float {
+            self.sliderSetValue.value = setValueSlider
+            lblShowValueSlider.text = "\(setValueSlider)"
+        }
     }
     
     @IBAction func disMiss(_ sender: Any) {
@@ -57,11 +91,15 @@ class GifAndCameraViewController: UIViewController {
         imagesPerSecond = TimeInterval(sender.value)
         timerVideoGif = Int(sender.value)
         lblShowValueSlider.text = "\(sender.value)"
+        let numberUserDerfault = UserDefaults.standard
+        numberUserDerfault.set(sender.value, forKey: "sliderValue")
+        numberUserDerfault.synchronize()
     }
     
     // MARK: CREATE GIF
     
     @IBAction func audioGif(_ sender: Any) {
+        checkCreateVideoAndGit = 1
         // hidden Create Gif and Create Video
         stackViewHidden.isHidden = true
         stackTimeSpeedHidden.isHidden = true
@@ -74,58 +112,95 @@ class GifAndCameraViewController: UIViewController {
          lblTut.isHidden = true
 
         // Create Gif
-        videogif.animationImages = arrimageVideo
-        videogif.animationDuration = TimeInterval(timerVideoGif)
-        if videoPlayerVc != nil {
-          videoPlayerVc.view.isHidden = true
-        }
-         videogif.isHidden = false
-         videogif.startAnimating()
+        let imageURL = UIImage.gifImageWithURL(urlGifString)
+        let imageView3 = UIImageView(image: imageURL)
+        imageView3.frame = CGRect(x: 0, y: 0, width: videogif.frame.width, height: videogif.frame.height)
+        imageView3.layer.cornerRadius = 17
+        videogif.addSubview(imageView3)
     }
 
     @IBAction func ShareAudioGifandVideo(_ sender: Any) {
-        // Share Video
-        let videoURL = URL(fileURLWithPath: urlString)
-        let activityItems: [Any] = [videoURL, "Check this out!"]
-        let activityController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
-        activityController.popoverPresentationController?.sourceView = view
-        activityController.popoverPresentationController?.sourceRect = view.frame
+        if checkCreateVideoAndGit == 0 {
+            let title = "Error"
+            let message = "Not File"
+            let view = MessageView.viewFromNib(layout: .cardView)
+            view.configureTheme(.error)
+            view.configureContent(title: title, body: message)
+            view.button?.isHidden = true
+            //Config
+            var config = SwiftMessages.Config()
+            config.duration = .automatic
+            SwiftMessages.show(config: config, view: view)
+        } else if checkCreateVideoAndGit == 1  {
+            btnReset.isHidden = false
+            // Share Gif
+           // let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0].appending("/animated.gif")
+            let shareURL: URL = URL(fileURLWithPath: urlGifString)
+            do {
+                let shareData: Data = try Data(contentsOf: shareURL)
+                let firstActivityItem: Array = [shareData]
+                let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: firstActivityItem, applicationActivities: nil)
+                self.present(activityViewController, animated: true, completion: nil)
+            } catch {
+                print("okok")
+            }
 
-        self.present(activityController, animated: true, completion: nil)
+        } else {
+            print("ajskljas")
+            btnReset.isHidden = false
+            // Share Video
+            let videoURL = URL(fileURLWithPath: urlString)
+            let activityItems: [Any] = [videoURL]
+            let activityController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+            activityController.popoverPresentationController?.sourceView = view
+            activityController.popoverPresentationController?.sourceRect = view.frame
 
-        // Share Gif
-//        let shareURL: URL = URL(fileURLWithPath: urlString)
-//        do {
-//            let shareData: Data = try Data(contentsOf: shareURL)
-//            let firstActivityItem: Array = [shareData]
-//            let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: firstActivityItem, applicationActivities: nil)
-//            self.present(activityViewController, animated: true, completion: nil)
-//        } catch {
-//            print("okok")
-//        }
-
+            self.present(activityController, animated: true, completion: nil)
+        }
     }
 
     @IBAction func saveCameraRoll(_ sender: Any) {
-        var placeHolder : PHObjectPlaceholder?
-        PHPhotoLibrary.shared().performChanges({
-            let creationRequest = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: URL(string: self.urlString)! )
-            placeHolder = creationRequest?.placeholderForCreatedAsset
+        if checkCreateVideoAndGit == 0 {
+            let title = "Error"
+            let message = "Not File"
+            let view = MessageView.viewFromNib(layout: .cardView)
+            view.configureTheme(.error)
+            view.configureContent(title: title, body: message)
+            view.button?.isHidden = true
+            //Config
+            var config = SwiftMessages.Config()
+            config.duration = .automatic
+            SwiftMessages.show(config: config, view: view)
+        } else if checkCreateVideoAndGit == 1 {
+                print("saveGif")
+        } else {
+            var placeHolder : PHObjectPlaceholder?
+            PHPhotoLibrary.shared().performChanges({
+                let creationRequest = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: URL(string: self.urlString)! )
+                placeHolder = creationRequest?.placeholderForCreatedAsset
 
-        }, completionHandler: { (success, error) in
-            if success {
-                let alertController = UIAlertController(title: "Your video was successfully saved", message: nil, preferredStyle: .alert)
-                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                alertController.addAction(defaultAction)
-                self.present(alertController, animated: true, completion: nil)
-                //               let result = PHAsset.fetchAssets(withLocalIdentifiers: [placeHolder!.localIdentifier], options: nil)
-                //                result.firstObject?.PHAsset(
-                //
-                //                    // this is the url to the saved asset
-                //
-                //                })
-            }
-        })
+            }, completionHandler: { (success, error) in
+                if success {
+                    let title = "Notification"
+                    let message = "Your video was successfully saved"
+                    let view = MessageView.viewFromNib(layout: .cardView)
+                    view.configureTheme(.success)
+                    view.configureContent(title: title, body: message)
+                    view.button?.isHidden = true
+                    //Config
+                    var config = SwiftMessages.Config()
+                    config.duration = .automatic
+                    SwiftMessages.show(config: config, view: view)
+
+                    //               let result = PHAsset.fetchAssets(withLocalIdentifiers: [placeHolder!.localIdentifier], options: nil)
+                    //                result.firstObject?.PHAsset(
+                    //
+                    //                    // this is the url to the saved asset
+                    //
+                    //                })
+                }
+            })
+        }
     }
 
       // MARK: PLAY VIDEO
@@ -162,9 +237,14 @@ class GifAndCameraViewController: UIViewController {
         videogif.isHidden = true
         videoPlayerVc.view.isHidden = false
         videoPlayerVc.player?.play()
+        self.loadingView.stopAnimating()
     }
 
     @IBAction func audioVideo(_ sender: Any) {
+        self.loadingView.type = .ballSpinFadeLoader
+        self.loadingView.color = #colorLiteral(red: 0.2588235294, green: 0.07843137255, blue: 0.9411764706, alpha: 1)
+        self.loadingView.startAnimating()
+        checkCreateVideoAndGit = 2
         // hidden Create Gif and Create Video
         stackViewHidden.isHidden = true
         stackTimeSpeedHidden.isHidden = true
@@ -180,6 +260,7 @@ class GifAndCameraViewController: UIViewController {
     }
 
     @IBAction func resetGifAndVideo(_ sender: UIButton) {
+        checkCreateVideoAndGit = 0
         // Show Create Gif and Create Video
         stackViewHidden.isHidden = false
         stackTimeSpeedHidden.isHidden = false
@@ -404,3 +485,186 @@ class GifAndCameraViewController: UIViewController {
 
 
 }
+
+extension UIImage {
+    func animatedGif(from images: [UIImage]) {
+        let fileProperties: CFDictionary = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: 0]]  as CFDictionary
+        let frameProperties: CFDictionary = [kCGImagePropertyGIFDictionary as String: [(kCGImagePropertyGIFDelayTime as String): 1.0]] as CFDictionary
+
+        let documentsDirectoryURL: URL? = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        let fileURL: URL? = documentsDirectoryURL?.appendingPathComponent("animated.gif")
+
+        if let url = fileURL as CFURL? {
+            if let destination = CGImageDestinationCreateWithURL(url, kUTTypeGIF, images.count, nil) {
+                CGImageDestinationSetProperties(destination, fileProperties)
+                for image in images {
+                    if let cgImage = image.cgImage {
+                        CGImageDestinationAddImage(destination, cgImage, frameProperties)
+                    }
+                }
+                if !CGImageDestinationFinalize(destination) {
+                    print("Failed to finalize the image destination")
+                }
+                print("Url = \(fileURL)")
+                urlGifString = "\(fileURL!)"
+            }
+        }
+    }
+
+    public class func gifImageWithData(_ data: Data) -> UIImage? {
+        guard let source = CGImageSourceCreateWithData(data as CFData, nil) else {
+            print("image doesn't exist")
+            return nil
+        }
+
+        return UIImage.animatedImageWithSource(source)
+    }
+
+    public class func gifImageWithURL(_ gifUrl:String) -> UIImage? {
+        guard let bundleURL:URL? = URL(string: gifUrl)
+            else {
+                print("image named \"\(gifUrl)\" doesn't exist")
+                return nil
+        }
+        guard let imageData = try? Data(contentsOf: bundleURL!) else {
+            print("image named \"\(gifUrl)\" into NSData")
+            return nil
+        }
+
+        return gifImageWithData(imageData)
+    }
+
+    public class func gifImageWithName(_ name: String) -> UIImage? {
+        guard let bundleURL = Bundle.main
+            .url(forResource: name, withExtension: "gif") else {
+                print("SwiftGif: This image named \"\(name)\" does not exist")
+                return nil
+        }
+        guard let imageData = try? Data(contentsOf: bundleURL) else {
+            print("SwiftGif: Cannot turn image named \"\(name)\" into NSData")
+            return nil
+        }
+
+        return gifImageWithData(imageData)
+    }
+
+    class func delayForImageAtIndex(_ index: Int, source: CGImageSource!) -> Double {
+        var delay = 0.1
+
+        let cfProperties = CGImageSourceCopyPropertiesAtIndex(source, index, nil)
+        let gifProperties: CFDictionary = unsafeBitCast(
+            CFDictionaryGetValue(cfProperties,
+                                 Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque()),
+            to: CFDictionary.self)
+
+        var delayObject: AnyObject = unsafeBitCast(
+            CFDictionaryGetValue(gifProperties,
+                                 Unmanaged.passUnretained(kCGImagePropertyGIFUnclampedDelayTime).toOpaque()),
+            to: AnyObject.self)
+        if delayObject.doubleValue == 0 {
+            delayObject = unsafeBitCast(CFDictionaryGetValue(gifProperties,
+                                                             Unmanaged.passUnretained(kCGImagePropertyGIFDelayTime).toOpaque()), to: AnyObject.self)
+        }
+
+        delay = delayObject as! Double
+
+        if delay < 0.1 {
+            delay = 0.1
+        }
+
+        return delay
+    }
+
+    class func gcdForPair(_ a: Int?, _ b: Int?) -> Int {
+        var a = a
+        var b = b
+        if b == nil || a == nil {
+            if b != nil {
+                return b!
+            } else if a != nil {
+                return a!
+            } else {
+                return 0
+            }
+        }
+
+        if a! < b! {
+            let c = a
+            a = b
+            b = c
+        }
+
+        var rest: Int
+        while true {
+            rest = a! % b!
+
+            if rest == 0 {
+                return b!
+            } else {
+                a = b
+                b = rest
+            }
+        }
+    }
+
+    class func gcdForArray(_ array: Array<Int>) -> Int {
+        if array.isEmpty {
+            return 1
+        }
+
+        var gcd = array[0]
+
+        for val in array {
+            gcd = UIImage.gcdForPair(val, gcd)
+        }
+
+        return gcd
+    }
+
+    class func animatedImageWithSource(_ source: CGImageSource) -> UIImage? {
+        let count = CGImageSourceGetCount(source)
+        var images = [CGImage]()
+        var delays = [Int]()
+
+        for i in 0..<count {
+            if let image = CGImageSourceCreateImageAtIndex(source, i, nil) {
+                images.append(image)
+            }
+
+            let delaySeconds = UIImage.delayForImageAtIndex(Int(i),
+                                                            source: source)
+            delays.append(Int(delaySeconds * 1000.0)) // Seconds to ms
+        }
+
+        let duration: Int = {
+            var sum = 0
+
+            for val: Int in delays {
+                sum += val
+            }
+
+            return sum
+        }()
+
+        let gcd = gcdForArray(delays)
+        var frames = [UIImage]()
+
+        var frame: UIImage
+        var frameCount: Int
+        for i in 0..<count {
+            frame = UIImage(cgImage: images[Int(i)])
+            frameCount = Int(delays[Int(i)] / gcd)
+
+            for _ in 0..<frameCount {
+                frames.append(frame)
+            }
+        }
+
+        let animation = UIImage.animatedImage(with: frames,
+                                              duration: Double(duration) / 1000.0)
+
+        return animation
+    }
+}
+
+
